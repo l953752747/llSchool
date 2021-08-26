af2753d388cb0d0a73d413d651e78b1c42b47212
ll <953752747@qq.com>
2021-08-26 11:37:35 +0800

    配置service_eduAppication

---
+++ llschool-parent/service/service_edu/src/main/java/com/llschool/eduService/EduApplication.java
@@ -0,0 +1,13 @@
+package com.llschool.eduService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class EduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class,args);
+    }
+}
