b4c40977ef4c3cd765ec491b8e62883d0441176b
ll <953752747@qq.com>
2021-08-26 09:52:51 +0800

    初始化结构配置+

---
+++ llschool-parent/src/main/java/com/example/llschoolparent/LlschoolParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.llschoolparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlschoolParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlschoolParentApplication.class, args);
+    }
+
+}
---
+++ llschool-parent/src/test/java/com/example/llschoolparent/LlschoolParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llschoolparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlschoolParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
